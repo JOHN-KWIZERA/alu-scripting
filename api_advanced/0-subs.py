9994565f83aed40946deacee85050310a062ba1d
JOHN-KWIZERA <j.kwizera@alustudent.com>
2024-07-05 20:15:28 +0200

    again

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """
-A script that use reddit api to outputs the
-number of subscribers of a certain subreddit.
+Script that queries subscribers on a given Reddit subreddit.
 """
 
-
-import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = json.loads(response.text)
-        return data['data']['subscribers']
-
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
