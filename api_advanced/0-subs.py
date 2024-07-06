cad99fba27f29129e09e9c0020186532ba6734d4
JOHN-KWIZERA <j.kwizera@alustudent.com>
2024-07-06 23:04:50 +0200

    new change

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
-"""Function to return number of subscribers"""
+"""
+Function to return number of subscribers of a subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """DOC"""
-    reddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    header = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url,
-                            headers=header
-                            )
-
-    if response.status_code == 200:
+    """
+    Return the number of subscribers for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit.
+    
+    Returns:
+        int: Number of subscribers if the request is successful, 0 otherwise.
+    """
+    reddit_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-agent': 'Mozilla/5.0'}
+    
+    try:
+        response = requests.get(reddit_url, headers=headers)
+        response.raise_for_status()
         data = response.json()
-        subs = data['data']['subscribers']
-        return subs
-    return 0
+        subscribers = data.get('data', {}).get('subscribers', 0)
+        return subscribers
+    except requests.RequestException:
+        return 0
+
+# Example usage:
+if __name__ == "__main__":
+    subreddit = "nonexistingsubreddit"
+    print(number_of_subscribers(subreddit))
