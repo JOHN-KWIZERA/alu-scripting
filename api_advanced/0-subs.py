d33586c24b679a90ddb13426466544d0ed4c0266
JOHN-KWIZERA <j.kwizera@alustudent.com>
2024-07-06 22:57:41 +0200

    good

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and
- returns the number of subscribers (all subscribers)"""
+"""Function to return number of subscribers"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    queries the Reddit API
-    returns the number of subscribers for a given reddit
-    """
-    import json
-    import requests
-    subreddit_URL = 'https://www.reddit.com/r/{}/about/.json'.format(
-        subreddit)
-    subreddit_info = requests.get(subreddit_URL,
-                                  headers={"user-agent": "user"},
-                                  allow_redirects=False).json()
-    if "data" not in subreddit_info:
-        return 0
-    subscribers = subreddit_info.get("data").get("subscribers")
-    return subscribers
+    """DOC"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    header = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url,
+                            headers=header
+                            )
+
+    if response.status_code == 200:
+        data = response.json()
+        subs = data['data']['subscribers']
+        return subs
+    return 0
