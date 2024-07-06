d3901cf3a08efe4c67ad74933bb59e717e596619
JOHN-KWIZERA <j.kwizera@alustudent.com>
2024-07-06 23:08:30 +0200

    new changes

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-"""
-Function to return number of subscribers of a subreddit.
-"""
-
+"""Function to return number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Return the number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
-    
-    Returns:
-        int: Number of subscribers if the request is successful, 0 otherwise.
-    """
-    reddit_url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-agent': 'Mozilla/5.0'}
-    
-    try:
-        response = requests.get(reddit_url, headers=headers)
-        response.raise_for_status()
-        data = response.json()
-        subscribers = data.get('data', {}).get('subscribers', 0)
-        return subscribers
-    except requests.RequestException:
-        return 0
+    """DOC"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-# Example usage:
-if __name__ == "__main__":
-    subreddit = "nonexistingsubreddit"
-    print(number_of_subscribers(subreddit))
+    header = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url,
+                            headers=header
+                            )
+
+    if response.status_code == 200:
+        data = response.json()
+        subs = data['data']['subscribers']
+        return subs
+    return 0
